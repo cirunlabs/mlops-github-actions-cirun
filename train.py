8e2884d4748df214a92a743ff38d95a7e9cf5d1f
Vishal Sharma <vs7375631@gmail.com>
2022-11-02 16:55:41 +0530

    testing tensorflow example model

--- train.py
+++ train.py
@@ -1,85 +1,57 @@
-import pandas as pd 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-import matplotlib.pyplot as plt
-import seaborn as sns
+import os
+
+import tensorflow.compat.v2 as tf
+import tensorflow_hub as hub
+
 import numpy as np
-# Set random seed
-seed = 42
+import cv2
+from IPython import display
+import math
 
-################################
-########## DATA PREP ###########
-################################
+# Load the model once from TF-Hub.
+hub_handle = 'https://tfhub.dev/deepmind/mil-nce/s3d/1'
+hub_model = hub.load(hub_handle)
 
-# Load in the data
-df = pd.read_csv("wine_quality.csv")
+def generate_embeddings(model, input_frames, input_words):
+  """Generate embeddings from the model from video frames and input words."""
+  # Input_frames must be normalized in [0, 1] and of the shape Batch x T x H x W x 3
+  vision_output = model.signatures['video'](tf.constant(tf.cast(input_frames, dtype=tf.float32)))
+  text_output = model.signatures['text'](tf.constant(input_words))
+  return vision_output['video_embedding'], text_output['text_embedding']
 
-# Split into train and test sections
-y = df.pop("quality")
-X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random_state=seed)
+  # @title Load example videos and define text queries  { display-mode: "form" }
 
-#################################
-########## MODELLING ############
-#################################
+video_1_url = 'https://upload.wikimedia.org/wikipedia/commons/b/b0/YosriAirTerjun.gif' # @param {type:"string"}
+video_2_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Guitar_solo_gif.gif' # @param {type:"string"}
+video_3_url = 'https://upload.wikimedia.org/wikipedia/commons/3/30/2009-08-16-autodrift-by-RalfR-gif-by-wau.gif' # @param {type:"string"}
 
-# Fit a model on the train section
-regr = RandomForestRegressor(max_depth=2, random_state=seed)
-regr.fit(X_train, y_train)
+video_1 = load_video(video_1_url)
+video_2 = load_video(video_2_url)
+video_3 = load_video(video_3_url)
+all_videos = [video_1, video_2, video_3]
 
-# Report training set score
-train_score = regr.score(X_train, y_train) * 100
-# Report test set score
-test_score = regr.score(X_test, y_test) * 100
+query_1_video = 'waterfall' # @param {type:"string"}
+query_2_video = 'playing guitar' # @param {type:"string"}
+query_3_video = 'car drifting' # @param {type:"string"}
+all_queries_video = [query_1_video, query_2_video, query_3_video]
+all_videos_urls = [video_1_url, video_2_url, video_3_url]
+display_video(all_videos_urls)
 
-# Write scores to a file
-with open("metrics.txt", 'w') as outfile:
-        outfile.write("Training variance explained: %2.1f%%\n" % train_score)
-        outfile.write("Test variance explained: %2.1f%%\n" % test_score)
+# Prepare video inputs.
+videos_np = np.stack(all_videos, axis=0)
 
+# Prepare text input.
+words_np = np.array(all_queries_video)
 
-##########################################
-##### PLOT FEATURE IMPORTANCE ############
-##########################################
-# Calculate feature importance in random forest
-importances = regr.feature_importances_
-labels = df.columns
-feature_df = pd.DataFrame(list(zip(labels, importances)), columns = ["feature","importance"])
-feature_df = feature_df.sort_values(by='importance', ascending=False,)
+# Generate the video and text embeddings.
+video_embd, text_embd = generate_embeddings(hub_model, videos_np, words_np)
 
-# image formatting
-axis_fs = 18 #fontsize
-title_fs = 22 #fontsize
-sns.set(style="whitegrid")
+# Scores between video and text is computed by dot products.
+all_scores = np.dot(text_embd, tf.transpose(video_embd))
 
-ax = sns.barplot(x="importance", y="feature", data=feature_df)
-ax.set_xlabel('Importance',fontsize = axis_fs) 
-ax.set_ylabel('Feature', fontsize = axis_fs)#ylabel
-ax.set_title('Random forest\nfeature importance', fontsize = title_fs)
-
-plt.tight_layout()
-plt.savefig("feature_importance.png",dpi=120) 
-plt.close()
-
-
-##########################################
-############ PLOT RESIDUALS  #############
-##########################################
-
-y_pred = regr.predict(X_test) + np.random.normal(0,0.25,len(y_test))
-y_jitter = y_test + np.random.normal(0,0.25,len(y_test))
-res_df = pd.DataFrame(list(zip(y_jitter,y_pred)), columns = ["true","pred"])
-
-ax = sns.scatterplot(x="true", y="pred",data=res_df)
-ax.set_aspect('equal')
-ax.set_xlabel('True wine quality',fontsize = axis_fs) 
-ax.set_ylabel('Predicted wine quality', fontsize = axis_fs)#ylabel
-ax.set_title('Residuals', fontsize = title_fs)
-
-# Make it pretty- square aspect ratio
-ax.plot([1, 10], [1, 10], 'black', linewidth=1)
-plt.ylim((2.5,8.5))
-plt.xlim((2.5,8.5))
-
-plt.tight_layout()
-plt.savefig("residuals.png",dpi=120) 
-
+# Display results.
+html = ''
+for i, words in enumerate(words_np):
+  html += display_query_and_results_video(words, all_videos_urls, all_scores[i, :])
+  html += '<br>'
+display.HTML(html)
