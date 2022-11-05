7bd0cd27a7d2bb8d38d980bf69f8142f9eb4c658
Vishal Sharma <vs7375631@gmail.com>
2022-11-06 04:58:58 +0530

    example1 test

--- train.py
+++ train.py
@@ -1,57 +1,228 @@
-import os
-
-import tensorflow.compat.v2 as tf
-import tensorflow_hub as hub
-
-import numpy as np
-import cv2
-from IPython import display
+import gym
 import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from copy import deepcopy
+from PIL import Image
 
-# Load the model once from TF-Hub.
-hub_handle = 'https://tfhub.dev/deepmind/mil-nce/s3d/1'
-hub_model = hub.load(hub_handle)
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.transforms as T
 
-def generate_embeddings(model, input_frames, input_words):
-  """Generate embeddings from the model from video frames and input words."""
-  # Input_frames must be normalized in [0, 1] and of the shape Batch x T x H x W x 3
-  vision_output = model.signatures['video'](tf.constant(tf.cast(input_frames, dtype=tf.float32)))
-  text_output = model.signatures['text'](tf.constant(input_words))
-  return vision_output['video_embedding'], text_output['text_embedding']
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
 
-  # @title Load example videos and define text queries  { display-mode: "form" }
+plt.ion()
+env = gym.make('CartPole-v0').unwrapped
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
-video_1_url = 'https://upload.wikimedia.org/wikipedia/commons/b/b0/YosriAirTerjun.gif' # @param {type:"string"}
-video_2_url = 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Guitar_solo_gif.gif' # @param {type:"string"}
-video_3_url = 'https://upload.wikimedia.org/wikipedia/commons/3/30/2009-08-16-autodrift-by-RalfR-gif-by-wau.gif' # @param {type:"string"}
 
-video_1 = load_video(video_1_url)
-video_2 = load_video(video_2_url)
-video_3 = load_video(video_3_url)
-all_videos = [video_1, video_2, video_3]
+class ReplayMemory(object):
 
-query_1_video = 'waterfall' # @param {type:"string"}
-query_2_video = 'playing guitar' # @param {type:"string"}
-query_3_video = 'car drifting' # @param {type:"string"}
-all_queries_video = [query_1_video, query_2_video, query_3_video]
-all_videos_urls = [video_1_url, video_2_url, video_3_url]
-display_video(all_videos_urls)
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
 
-# Prepare video inputs.
-videos_np = np.stack(all_videos, axis=0)
+    def push(self, *args):
+        """Saves a transition."""
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
 
-# Prepare text input.
-words_np = np.array(all_queries_video)
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
 
-# Generate the video and text embeddings.
-video_embd, text_embd = generate_embeddings(hub_model, videos_np, words_np)
+    def __len__(self):
+        return len(self.memory)
+class DQN(nn.Module):
 
-# Scores between video and text is computed by dot products.
-all_scores = np.dot(text_embd, tf.transpose(video_embd))
+    def __init__(self):
+        super(DQN, self).__init__()
+        
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+        self.head = nn.Linear(448, 2)
 
-# Display results.
-html = ''
-for i, words in enumerate(words_np):
-  html += display_query_and_results_video(words, all_videos_urls, all_scores[i, :])
-  html += '<br>'
-display.HTML(html)
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        return self.head(x.view(x.size(0), -1))
+resize = T.Compose([T.ToPILImage(),
+                    T.Scale(40, interpolation=Image.CUBIC),
+                    T.ToTensor()])
+
+# This width is based on the code from gym.
+screen_width = 600
+
+
+def get_cart_location():
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+
+def get_screen():
+    screen = env.render(mode='rgb_array').transpose(
+        (2, 0, 1))  # transpose into torch order (CHW)
+    # Strip off the top and bottom of the screen
+    screen = screen[:, 160:320]
+    view_width = 320
+    cart_location = get_cart_location()
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+    # Strip off the edges, so that we have a square image centered on a cart
+    screen = screen[:, :, slice_range]
+    # Convert to float, rescare, convert to torch tensor
+    # (this doesn't require a copy)
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+    # Resize, and add a batch dimension (BCHW)
+    return resize(screen).unsqueeze(0).type(Tensor)
+
+env.reset()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+           interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+
+model = DQN()
+
+if use_cuda:
+    model.cuda()
+
+optimizer = optim.RMSprop(model.parameters())
+memory = ReplayMemory(10000)
+steps_done = 0
+
+# predict agent action from input state
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        return model(Variable(state, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+    else:
+        return LongTensor([[random.randrange(2)]])
+def optimize_model():
+    
+    # only train when we have enough transitions for at least one batch
+    if len(memory) < BATCH_SIZE:
+        return
+    
+    # collect a batch of sample transitions from ReplayMemory
+    transitions = memory.sample(BATCH_SIZE)
+    
+    # Transpose the batch (see http://stackoverflow.com/a/19343/3343043 for detailed explanation).
+    batch = Transition(*zip(*transitions))
+
+    # Compute a mask of non-final states and concatenate the batch elements
+    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None,
+                                          batch.next_state)))
+
+    # We don't want to backprop through the expected action values and volatile
+    # will save us on temporarily changing the model parameters'
+    # requires_grad to False!
+    non_final_next_states = Variable(torch.cat([s for s in batch.next_state
+                                                if s is not None]),
+                                     volatile=True)
+    
+    state_batch = Variable(torch.cat(batch.state))
+    action_batch = Variable(torch.cat(batch.action))
+    reward_batch = Variable(torch.cat(batch.reward))
+
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken
+    state_action_values = model(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
+    next_state_values[non_final_mask] = model(non_final_next_states).max(1)[0]
+    # Now, we don't want to mess up the loss with a volatile flag, so let's
+    # clear it. After this, we'll just end up with a Variable that has
+    # requires_grad=False
+    next_state_values.volatile = False
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    for param in model.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+num_episodes = 50
+for i_episode in range(num_episodes):
+    # Initialize the environment and state
+    env.reset()
+    last_screen = get_screen()
+    current_screen = get_screen()
+    state = current_screen - last_screen
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        _, reward, done, _ = env.step(action[0, 0])
+        reward = Tensor([reward])
+
+        # Observe new state
+        last_screen = current_screen
+        current_screen = get_screen()
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        optimize_model()
+        if done:
+            print('Episode %02i, Duration %i' % (i_episode, t+1))
+            break
+
+print('Complete')
+env.render(close=True)
+env.close()
+plt.ioff()
+plt.show()
